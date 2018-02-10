4f4ab0a1ccb622b324c77ff7e856e72b4ff7d0de
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-02-10 12:15:30 -0700

    header and routing working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+} from 'react-router-dom'
+
+import About from './About'
+import View from './View'
+import Create from './Create'
+
+const Header = styled.header`
+  display: flex;
+  width: 100%;
+  height: 50px;
+  background-color: blue;
+  justify-content: flex-end;
+`
+const NavButtonContainer = styled.div`
+  display: flex;
+`
+const NavButton = styled(NavLink)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100px;
+  border: solid 1px red;
+  text-decoration: none;
+  color: black;
+`
+
+const activeStyle = {
+  backgroundColor: 'red',
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Header>
+            <NavButtonContainer>
+              <NavButton
+                exact
+                to="/create"
+                activeStyle={ activeStyle }
+              >
+                Create
+              </NavButton>
+              <NavButton
+                exact
+                to="/view"
+                activeStyle={ activeStyle }
+              >
+                Find
+              </NavButton>
+            </NavButtonContainer>
+          </Header>
+          <Route exact path="/" component={About}/>
+          <Route path="/create" component={Create}/>
+          <Route path="/view" component={View}/>
+        </div>
+      </Router>
     );
   }
 }
